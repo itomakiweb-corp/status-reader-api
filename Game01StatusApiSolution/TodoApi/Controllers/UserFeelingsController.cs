1988f58536f85ff58d79e3f2876bd031996e638c
o4get <4get3not.k.o.o.l@gmail.com>
2018-10-05 06:41:30 +0900

    init

---
+++ Game01StatusApiSolution/TodoApi/Controllers/UserFeelingsController.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TodoApi.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace TodoApi.Controllers
+{
+    [Route("api/UserFeelings")]
+    [ApiController]
+    public class UserFeelingsController : Controller
+    {
+        private readonly UserFeelingContext _context;
+        public UserFeelingsController(UserFeelingContext context)
+        {
+            _context = context;
+
+            //if (_context.UserFeelings.Count() == 0)
+            //{
+            //    _context.UserFeelings.Add(new UserFeeling { Name = "item1" });
+            //    _context.SaveChanges();
+            //}
+        }
+
+        // GET: api/<controller>
+        [HttpGet]
+        public ActionResult<List<UserFeeling>> GetAll()
+        {
+            var l = _context.UserFeelings.ToList();
+            return l;
+        }
+
+        // GET api/<controller>/5
+        [HttpGet("{id}", Name = "GetFeelings")]
+        public ActionResult<UserFeeling> GetById(long id)
+        {
+            var item = _context.UserFeelings.Find(id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+            return item;
+        }
+
+        // POST api/<controller>
+        [HttpPost]
+        public IActionResult Create(UserFeeling item)
+        {
+            if (item.Id <= 0)
+            {
+                if (_context.UserFeelings.Count() == 0)
+                {
+                    item.Id = 1;
+                }
+                else
+                {
+                    var last = _context.UserFeelings.Last();
+                    if (last != null)
+                    {
+                        item.Id = last.Id++;
+                    }
+                }
+            }
+            _context.UserFeelings.Add(item);
+            _context.SaveChanges();
+            return Ok();
+            //return CreatedAtRoute("GetFeellings", new { id = item.Id }, item);
+        }
+
+        //// PUT api/<controller>/5
+        //[HttpPut("{id}")]
+        //public void Put(int id, [FromBody]string value)
+        //{
+        //}
+
+        //// DELETE api/<controller>/5
+        [HttpDelete("{id}")]
+        public IActionResult Delete(long id)
+        {
+            var item = _context.UserFeelings.Find(id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+
+            _context.UserFeelings.Remove(item);
+            _context.SaveChanges();
+            return Ok();
+        }
+    }
+}
---
+++ Game01StatusApiSolution/TodoApi/Models/UserFeeling.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+
+namespace TodoApi.Models
+{
+    public class UserFeeling
+    {
+        public long Id { get; set; }
+        public string UserName { get; set; }
+        public string Comment1 { get; set; }
+        public string Comment2 { get; set; }
+        public string Comment3 { get; set; }
+        public string IssuedTime { get; set; }
+    }
+
+    public class UserFeelingContext : DbContext
+    {
+        public UserFeelingContext(DbContextOptions<UserFeelingContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<UserFeeling> UserFeelings { get; set; }
+    }
+}
